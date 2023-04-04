08c0d8814fd674b1e024da9fdb0039b12271318e
Miguel Angel <miguel.ampudia@outlook.es>
2023-04-03 21:14:18 -0500

    update file

--- ProjectTest.API/Program.cs
+++ ProjectTest.API/Program.cs
@@ -11,13 +11,13 @@ var port = Environment.GetEnvironmentVariable("PORT")??"8080";
 app.MapGet("/", () =>
 {
     app.Logger.LogInformation("Initial Home Get");
-    return "GET - Net 6 Running ... 03042023-v1.0.0 CI/CD in TRAVIS v1.0.0 AFORO255";
+    return "GET - Net 6 Running ... 03042023-v1.0.0 CI/CD in TRAVIS v1.0.0 MIGUEL ANGEL AMPUDIA";
 });
 
 app.MapPost("/", () =>
 {
     app.Logger.LogInformation("Initial Home Post");
-    return "POST - Net 6 Running ... 03042023-v1.0.0 CI/CD in TRAVIS v1.0.0 AFORO255";
+    return "POST - Net 6 Running ... 03042023-v1.0.0 CI/CD in TRAVIS v1.0.0 MIGUEL ANGEL AMPUDIA";
 });
 
 app.Run($"http://0.0.0.0:{port}");
